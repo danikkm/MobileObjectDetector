da11763aafaa64a5ee39c6a9eb8d4d7a099183f0
Daniel Dluznevskij <danik@Daniels-MacBook-Pro.local>
2020-12-16 17:11:12 +0200

    initial commit

---
+++ MobileObjectDetector/Base/AppCoordinator.swift
@@ -0,0 +1,23 @@
+//
+//  AppCoordinator.swift
+//  MobileObjectDetector
+//
+//  Created by Daniel Dluznevskij on 12/16/20.
+//
+
+import UIKit
+
+class AppCoordinator {
+    private let window: UIWindow?
+    
+    init (window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        let viewController = ObjectRecognitionViewController.instantiate()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ MobileObjectDetector/Base/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  MobileObjectDetector
+//
+//  Created by Daniel Dluznevskij on 12/16/20.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    var appCoordinator: AppCoordinator?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+//        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        self.window = UIWindow(windowScene: scene)
+        appCoordinator = AppCoordinator(window: self.window ?? UIWindow(frame: UIScreen.main.bounds))
+        appCoordinator?.start()
+    }
+}
+
